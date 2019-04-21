572604008ac76e0dd24d538461fc04a446983444
Andrei Mesina <andrei.mesina@my.fmi.unibuc.ro>
2019-04-22 02:55:20 +0300

    Update Database class

--- app/src/main/java/com/andreimesina/kitesurfingworldwide/data/SpotDatabase.java
+++ app/src/main/java/com/andreimesina/kitesurfingworldwide/data/SpotDatabase.java
@@ -1,6 +1,9 @@
 package com.andreimesina.kitesurfingworldwide.data;
 
+import android.content.Context;
+
 import androidx.room.Database;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
 import com.andreimesina.kitesurfingworldwide.data.model.Spot;
@@ -13,4 +16,14 @@ public abstract class SpotDatabase extends RoomDatabase {
 
     public abstract SpotDao getSpotDao();
 
+    public synchronized SpotDatabase getInstance(Context context) {
+        if(instance == null) {
+            instance = Room.databaseBuilder(context.getApplicationContext(), SpotDatabase.class,
+                    "Kitesurfing_Worldwide")
+                    .fallbackToDestructiveMigration()
+                    .build();
+        }
+
+        return instance;
+    }
 }
