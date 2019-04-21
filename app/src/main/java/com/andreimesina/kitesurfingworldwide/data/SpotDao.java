dfb93b40eb6b04d7de232a95da85df5a1cadfeeb
Andrei Mesina <andrei.mesina@my.fmi.unibuc.ro>
2019-04-22 02:40:04 +0300

    Update Spot Data Access Object

--- app/src/main/java/com/andreimesina/kitesurfingworldwide/data/SpotDao.java
+++ app/src/main/java/com/andreimesina/kitesurfingworldwide/data/SpotDao.java
@@ -1,8 +1,31 @@
 package com.andreimesina.kitesurfingworldwide.data;
 
+import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.andreimesina.kitesurfingworldwide.data.model.Spot;
+
+import java.util.List;
 
 @Dao
 public interface SpotDao {
 
+    @Insert
+    void insert(Spot spot);
+
+    @Update
+    void update(Spot spot);
+
+    @Delete
+    void delete(Spot spot);
+
+    @Query("DELETE FROM Spot")
+    void deleteAll();
+
+    @Query("SELECT * FROM Spot ORDER BY id DESC")
+    LiveData<List<Spot>> getAll();
 }
