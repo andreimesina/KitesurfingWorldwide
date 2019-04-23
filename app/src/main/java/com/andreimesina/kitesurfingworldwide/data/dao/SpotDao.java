97580a670201aa9412f8ecb175faee770cba5b2c
Andrei Mesina <andrei.mesina@my.fmi.unibuc.ro>
2019-04-23 20:42:16 +0300

    Update Spot Data Access Object

--- app/src/main/java/com/andreimesina/kitesurfingworldwide/data/dao/SpotDao.java
+++ app/src/main/java/com/andreimesina/kitesurfingworldwide/data/dao/SpotDao.java
@@ -3,7 +3,9 @@ package com.andreimesina.kitesurfingworldwide.data.dao;
 import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
 import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
+import androidx.room.Update;
 
 import com.andreimesina.kitesurfingworldwide.data.model.Spot;
 
@@ -12,18 +14,21 @@ import java.util.List;
 @Dao
 public interface SpotDao {
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     void insertAllSpots(List<Spot> spots);
-
-    @Query("UPDATE Spot SET isFavorite = 1 WHERE id = :spotId")
-    void addSpotToFavorites(int spotId);
-
-    @Query("UPDATE Spot SET isFavorite = 0 WHERE id = :spotId")
-    void removeSpotFromFavorites(int spotId);
 
     @Query("SELECT * FROM Spot ORDER BY id DESC")
     LiveData<List<Spot>> getAllSpots();
 
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    void updateSpotDetails(Spot spot);
+
     @Query("SELECT * FROM Spot WHERE id = :spotId")
-    LiveData<Spot> getSpotDetails(int spotId);
+    LiveData<Spot> getSpotDetails(String spotId);
+
+    @Query("UPDATE Spot SET isFavorite = 1 WHERE id = :spotId")
+    void addSpotToFavorites(String spotId);
+
+    @Query("UPDATE Spot SET isFavorite = 0 WHERE id = :spotId")
+    void removeSpotFromFavorites(String spotId);
 }
