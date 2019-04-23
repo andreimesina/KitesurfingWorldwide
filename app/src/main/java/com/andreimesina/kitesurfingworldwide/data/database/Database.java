42b2211bedfaa46c932b0acb8431e6642aaf2f87
Andrei Mesina <andrei.mesina@my.fmi.unibuc.ro>
2019-04-23 20:43:23 +0300

    Refactor Database class

--- app/src/main/java/com/andreimesina/kitesurfingworldwide/data/database/Database.java
+++ app/src/main/java/com/andreimesina/kitesurfingworldwide/data/database/Database.java
@@ -1,24 +1,24 @@
-package com.andreimesina.kitesurfingworldwide.data;
+package com.andreimesina.kitesurfingworldwide.data.database;
 
 import android.content.Context;
 
-import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
+import com.andreimesina.kitesurfingworldwide.data.dao.SpotDao;
 import com.andreimesina.kitesurfingworldwide.data.model.Spot;
 
-@Database(entities = Spot.class, version = 1)
-public abstract class SpotDatabase extends RoomDatabase {
+@androidx.room.Database(entities = Spot.class, version = 1)
+public abstract class Database extends RoomDatabase {
 
-    private static SpotDatabase instance;
+    private static Database instance;
     private static SpotDao spotDao;
 
     public abstract SpotDao getSpotDao();
 
-    public synchronized SpotDatabase getInstance(Context context) {
+    public static synchronized Database getInstance(Context context) {
         if(instance == null) {
-            instance = Room.databaseBuilder(context.getApplicationContext(), SpotDatabase.class,
+            instance = Room.databaseBuilder(context.getApplicationContext(), Database.class,
                     "Kitesurfing_Worldwide")
                     .fallbackToDestructiveMigration()
                     .build();
