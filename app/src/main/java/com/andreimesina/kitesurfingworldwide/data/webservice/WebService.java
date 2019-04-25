82af0488a1d27c2f212fd64cd463199881e7b6b2
Andrei Mesina <andrei.mesina@my.fmi.unibuc.ro>
2019-04-26 00:34:54 +0300

    Implemented an abstract layer of the API calls in WebService class

--- app/src/main/java/com/andreimesina/kitesurfingworldwide/data/webservice/WebService.java
+++ app/src/main/java/com/andreimesina/kitesurfingworldwide/data/webservice/WebService.java
@@ -1,5 +1,16 @@
 package com.andreimesina.kitesurfingworldwide.data.webservice;
 
+import com.andreimesina.kitesurfingworldwide.core.ServiceProvider;
+import com.andreimesina.kitesurfingworldwide.data.model.Profile;
+import com.andreimesina.kitesurfingworldwide.data.model.Spot;
+import com.andreimesina.kitesurfingworldwide.data.model.SpotFilter;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -7,19 +18,54 @@ public class WebService {
 
     private static final String API_BASE_URL = "https://internship-2019.herokuapp.com";
 
-    private static WebService instance;
-
-    private Api api;
+    private ApiService api;
 
     public WebService() {
+        HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor();
+        httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
         api = new Retrofit.Builder()
                 .baseUrl(API_BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
+                .client(new OkHttpClient.Builder()
+                        .callTimeout(10, TimeUnit.SECONDS)
+                        .addInterceptor(httpLoggingInterceptor)
+                        .build())
                 .build()
-                .create(Api.class);
+                .create(ApiService.class);
     }
 
-    public Api getApi() {
-        return api;
+    public Call<Profile> createProfile() {
+        return api.createProfile(
+                ServiceProvider.getInstance().getAuthManager().getProfile().getEmail());
+    }
+
+    public Call<List<Spot>> getAllSpots(SpotFilter spotFilter) {
+        return api.getAllSpots(
+                ServiceProvider.getInstance().getAuthManager().getProfile().getToken(),
+                spotFilter);
+    }
+
+    public Call<Spot> getSpotDetails(String spotId) {
+        return api.getSpotDetails(
+                ServiceProvider.getInstance().getAuthManager().getProfile().getToken(),
+                spotId);
+    }
+
+    public Call<List<String>> getAllSpotCountries() {
+        return api.getAllSpotCountries(
+                ServiceProvider.getInstance().getAuthManager().getProfile().getToken());
+    }
+
+    public Call<Void> addSpotToFavorites(String spotId) {
+        return api.addSpotToFavorites(
+                ServiceProvider.getInstance().getAuthManager().getProfile().getToken(),
+                spotId);
+    }
+
+    public Call<Void> removeSpotFromFavorites(String spotId) {
+        return api.removeSpotFromFavorites(
+                ServiceProvider.getInstance().getAuthManager().getProfile().getToken(),
+                spotId);
     }
 }
