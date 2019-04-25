62cd9c7f6ac5ff64d71aa188bf31bbb83043ecc4
Andrei Mesina <andrei.mesina@my.fmi.unibuc.ro>
2019-04-26 00:42:20 +0300

    Refactor Repository class

--- app/src/main/java/com/andreimesina/kitesurfingworldwide/data/repository/Repository.java
+++ app/src/main/java/com/andreimesina/kitesurfingworldwide/data/repository/Repository.java
@@ -1,88 +1,107 @@
 package com.andreimesina.kitesurfingworldwide.data.repository;
 
 import android.app.Application;
-import android.os.AsyncTask;
 import android.widget.Toast;
 
 import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
 
+import com.andreimesina.kitesurfingworldwide.core.ServiceProvider;
 import com.andreimesina.kitesurfingworldwide.data.dao.SpotDao;
 import com.andreimesina.kitesurfingworldwide.data.database.Database;
 import com.andreimesina.kitesurfingworldwide.data.model.Profile;
 import com.andreimesina.kitesurfingworldwide.data.model.Spot;
-import com.andreimesina.kitesurfingworldwide.data.model.SpotDetails;
-import com.andreimesina.kitesurfingworldwide.data.webservice.Api;
+import com.andreimesina.kitesurfingworldwide.data.model.SpotFilter;
 import com.andreimesina.kitesurfingworldwide.data.webservice.WebService;
-import com.andreimesina.kitesurfingworldwide.utils.Utils;
+
+import org.threeten.bp.Instant;
+import org.threeten.bp.temporal.ChronoUnit;
 
 import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
+import timber.log.Timber;
 
 public class Repository {
 
     private Application app;
 
-    private Api apiService;
-    private SpotDao spotDao;
-    private LiveData<List<Spot>> allSpots;
+    private WebService webService;
+    private SpotDao dao;
+
+    private Executor executor = Executors.newCachedThreadPool();
+
+    private Instant spotsSyncedAt = Instant.EPOCH;
 
     public Repository(Application application) {
         app = application;
 
-        Database database = Database.getInstance(application);
-        spotDao = database.getSpotDao();
+        webService = new WebService();
 
-        WebService webService = new WebService();
-        apiService = webService.getApi();
-
-        createUser(Profile.getInstance().getEmail());
-        allSpots = getAllSpotsFromDb();
+        Database database = Database.getInstance(app);
+        dao = database.getSpotDao();
     }
 
-    public void createUser(String email) {
-        apiService.createProfile(email)
+    public void createProfile() {
+        webService.createProfile()
                 .enqueue(new Callback<Profile>() {
                     @Override
                     public void onResponse(Call<Profile> call, Response<Profile> response) {
                         if(response.isSuccessful()) {
-                            Profile.getInstance().setEmail(response.body().getEmail());
-                            Profile.getInstance().setToken(response.body().getToken());
+                            ServiceProvider.getInstance().getAuthManager().setProfile(
+                                    response.body());
+                        } else {
+                            Toast.makeText(
+                                    app, "Could not authenticate.", Toast.LENGTH_SHORT)
+                                    .show();
+                            Timber.d("Could not authenticate.");
                         }
                     }
 
                     @Override
                     public void onFailure(Call<Profile> call, Throwable t) {
-                        Toast.makeText(app, "Authentication failed", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(app, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                        Timber.d("Authentication failed.");
                     }
                 });
     }
 
     private void insertAllSpots(List<Spot> spots) {
-        new InsertAllSpotsAsyncTask(spotDao).execute(spots);
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                dao.insertAllSpots(spots);
+            }
+        });
     }
 
-    public LiveData<List<Spot>> getAllSpots(SpotDetails spotDetails) {
-        if(Utils.isNetworkConnected(app)) {
-            return getAllSpotsFromApi(spotDetails);
-        }
+    public LiveData<List<Spot>> getAllSpots(SpotFilter spotFilter) {
+        syncSpots(spotFilter);
 
         return getAllSpotsFromDb();
     }
 
-    private LiveData<List<Spot>> getAllSpotsFromApi(SpotDetails spotDetails) {
-        final MutableLiveData<List<Spot>> data = new MutableLiveData<>();
+    private void syncSpots(SpotFilter spotFilter) {
+        if(spotsSyncedAt.isAfter(Instant.now().minus(10, ChronoUnit.MINUTES))) {
+            Timber.d("Spots have been already synced in the last 10 minutes. Skipping...");
+            return ;
+        }
 
-        apiService.getAllSpots(Profile.getInstance().getToken(), spotDetails)
+        webService.getAllSpots(spotFilter)
                 .enqueue(new Callback<List<Spot>>() {
                     @Override
                     public void onResponse(Call<List<Spot>> call, Response<List<Spot>> response) {
                         if(response.isSuccessful()) {
-                            data.setValue(response.body());
-                            insertAllSpots(data.getValue());
+                            spotsSyncedAt = Instant.now();
+                            insertAllSpots(response.body());
+                        } else {
+                            Toast.makeText(
+                                    app, "Could not fetch latest spots.", Toast.LENGTH_SHORT)
+                                    .show();
+                            Timber.d("Could not fetch latest spots.");
                         }
                     }
 
@@ -90,145 +109,118 @@ public class Repository {
                     public void onFailure(Call<List<Spot>> call, Throwable t) {
                         Toast.makeText(app, "Failed to fetch latest spots", Toast.LENGTH_SHORT)
                                 .show();
-                        data.setValue(getAllSpotsFromDb().getValue());
+                        Timber.d("Failed to fetch latest spots.");
                     }
                 });
-
-        return data;
     }
 
     private LiveData<List<Spot>> getAllSpotsFromDb() {
-        return spotDao.getAllSpots();
+        return dao.getAllSpots();
     }
 
     private void updateSpotDetails(Spot spot) {
-        new UpdateSpotDetailsAsyncTask(spotDao).execute(spot);
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                dao.updateSpotDetails(spot);
+            }
+        });
     }
 
     public LiveData<Spot> getSpotDetails(String spotId) {
-        if(Utils.isNetworkConnected(app)) {
-            return getSpotDetailsFromApi(spotId);
-        }
+        syncSpotDetails(spotId);
 
         return getSpotDetailsFromDb(spotId);
     }
 
-    private LiveData<Spot> getSpotDetailsFromApi(String spotId) {
-        MutableLiveData<Spot> data = new MutableLiveData<>();
-
-        apiService.getSpotDetails(Profile.getInstance().getToken(), spotId)
+    private void syncSpotDetails(String spotId) {
+        webService.getSpotDetails(spotId)
                 .enqueue(new Callback<Spot>() {
                     @Override
                     public void onResponse(Call<Spot> call, Response<Spot> response) {
                         if(response.isSuccessful()) {
-                            data.setValue(response.body());
                             updateSpotDetails(response.body());
+                        } else {
+                            Timber.d("Could not sync spot details. spotId: %s", spotId);
                         }
                     }
 
                     @Override
                     public void onFailure(Call<Spot> call, Throwable t) {
-
+                        Timber.d("Failed to sync spot details. spotId: %s", spotId);
                     }
                 });
-
-        return data;
     }
 
     private LiveData<Spot> getSpotDetailsFromDb(String spotId) {
-        return spotDao.getSpotDetails(spotId);
+        return dao.getSpotDetails(spotId);
     }
 
     public void addSpotToFavorites(String spotId) {
-        new AddSpotToFavoritesAsyncTask(spotDao).execute(spotId);
-
-        apiService.addSpotToFavorites(Profile.getInstance().getToken(), spotId)
+        webService.addSpotToFavorites(spotId)
                 .enqueue(new Callback<Void>() {
                     @Override
                     public void onResponse(Call<Void> call, Response<Void> response) {
-                        Toast.makeText(app, "Added to favorites", Toast.LENGTH_SHORT).show();
+                        if(response.isSuccessful()) {
+                            Toast.makeText(app, "Added to favorites", Toast.LENGTH_SHORT).show();
+                            addSpotToFavoritesDb(spotId);
+                        } else {
+                            Toast.makeText(app, "Could not add to favorites.", Toast.LENGTH_SHORT)
+                                    .show();
+                            Timber.d("Could not add to favorites. spotId: %s", spotId);
+                        }
                     }
 
                     @Override
                     public void onFailure(Call<Void> call, Throwable t) {
                         Toast.makeText(app, "Failed to add to favorites", Toast.LENGTH_SHORT)
                                 .show();
+                        Timber.d("Failed to add to favorites. spotId: %s", spotId);
                     }
                 });
     }
 
-    public void removeSpotFromFavorites(String spotId) {
-        new RemoveSpotFromFavoritesAsyncTask(spotDao).execute(spotId);
+    private void addSpotToFavoritesDb(String spotId) {
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                dao.addSpotToFavorites(spotId);
+            }
+        });
+    }
 
-        apiService.removeSpotFromFavorites(Profile.getInstance().getToken(), spotId)
+    public void removeSpotFromFavorites(String spotId) {
+        webService.removeSpotFromFavorites(spotId)
                 .enqueue(new Callback<Void>() {
                     @Override
                     public void onResponse(Call<Void> call, Response<Void> response) {
-                        Toast.makeText(app, "Removed from favorites", Toast.LENGTH_SHORT)
-                                .show();
+                        if(response.isSuccessful()) {
+                            Toast.makeText(app, "Removed from favorites", Toast.LENGTH_SHORT)
+                                    .show();
+                            removeSpotFromFavoritesDb(spotId);
+                        } else {
+                            Toast.makeText(
+                                    app, "Could not remove from favorites.", Toast.LENGTH_SHORT)
+                                    .show();
+                            Timber.d("Could not remove from favorites. spotId: %s", spotId);
+                        }
                     }
 
                     @Override
                     public void onFailure(Call<Void> call, Throwable t) {
                         Toast.makeText(app, "Failed to remove from favorites",
                                 Toast.LENGTH_SHORT).show();
+                        Timber.d("Failed to remove from favorites. spotId: %s", spotId);
                     }
                 });
     }
 
-    private static class InsertAllSpotsAsyncTask extends AsyncTask<List<Spot>, Void, Void> {
-        private SpotDao spotDao;
-
-        private InsertAllSpotsAsyncTask(SpotDao spotDao) {
-            this.spotDao = spotDao;
-        }
-
-        @Override
-        protected Void doInBackground(List<Spot>... lists) {
-            spotDao.insertAllSpots(lists[0]);
-            return null;
-        }
-    }
-
-    private static class UpdateSpotDetailsAsyncTask extends AsyncTask<Spot, Void, Void> {
-        private SpotDao spotDao;
-
-        private UpdateSpotDetailsAsyncTask(SpotDao spotDao) {
-            this.spotDao = spotDao;
-        }
-
-        @Override
-        protected Void doInBackground(Spot... spots) {
-            spotDao.updateSpotDetails(spots[0]);
-            return null;
-        }
-    }
-
-    private static class AddSpotToFavoritesAsyncTask extends AsyncTask<String, Void, Void> {
-        private SpotDao spotDao;
-
-        private AddSpotToFavoritesAsyncTask(SpotDao spotDao) {
-            this.spotDao = spotDao;
-        }
-
-        @Override
-        protected Void doInBackground(String... strings) {
-            spotDao.addSpotToFavorites(strings[0]);
-            return null;
-        }
-    }
-
-    private static class RemoveSpotFromFavoritesAsyncTask extends AsyncTask<String, Void, Void> {
-        private SpotDao spotDao;
-
-        private RemoveSpotFromFavoritesAsyncTask(SpotDao spotDao) {
-            this.spotDao = spotDao;
-        }
-
-        @Override
-        protected Void doInBackground(String... strings) {
-            spotDao.removeSpotFromFavorites(strings[0]);
-            return null;
-        }
+    private void removeSpotFromFavoritesDb(String spotId) {
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                dao.removeSpotFromFavorites(spotId);
+            }
+        });
     }
 }
