de7182f5bfd21dfd0d8317ce4db7b4ee4f23c8eb
Andrei Mesina <andrei.mesina@my.fmi.unibuc.ro>
2019-04-26 05:05:07 +0300

    Make Spot id a String

--- app/src/main/java/com/andreimesina/kitesurfingworldwide/data/model/Spot.java
+++ app/src/main/java/com/andreimesina/kitesurfingworldwide/data/model/Spot.java
@@ -1,29 +1,41 @@
 package com.andreimesina.kitesurfingworldwide.data.model;
 
+import androidx.annotation.NonNull;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
+
+import com.google.gson.annotations.SerializedName;
 
 @Entity
 public class Spot {
 
     @PrimaryKey
-    private int id;
+    @NonNull
+    @SerializedName("id")
+    private String id;
 
+    @SerializedName("name")
     private String name;
 
+    @SerializedName("longitude")
     private float longitude;
 
+    @SerializedName("latitude")
     private float latitude;
 
+    @SerializedName("windProbability")
     private int windProbability;
 
+    @SerializedName("country")
     private String country;
 
+    @SerializedName("whenToGo")
     private String whenToGo;
 
+    @SerializedName("isFavorite")
     private boolean isFavorite;
 
-    public Spot(int id, String name, float longitude, float latitude, int windProbability,
+    public Spot(String id, String name, float longitude, float latitude, int windProbability,
                 String country, String whenToGo, boolean isFavorite) {
         this.id = id;
         this.name = name;
@@ -35,35 +47,67 @@ public class Spot {
         this.isFavorite = isFavorite;
     }
 
-    public int getId() {
+    public String getId() {
         return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 
     public String getName() {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public float getLongitude() {
         return longitude;
+    }
+
+    public void setLongitude(float longitude) {
+        this.longitude = longitude;
     }
 
     public float getLatitude() {
         return latitude;
     }
 
+    public void setLatitude(float latitude) {
+        this.latitude = latitude;
+    }
+
     public int getWindProbability() {
         return windProbability;
+    }
+
+    public void setWindProbability(int windProbability) {
+        this.windProbability = windProbability;
     }
 
     public String getCountry() {
         return country;
     }
 
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
     public String getWhenToGo() {
         return whenToGo;
     }
 
+    public void setWhenToGo(String whenToGo) {
+        this.whenToGo = whenToGo;
+    }
+
     public boolean isFavorite() {
         return isFavorite;
+    }
+
+    public void setFavorite(boolean favorite) {
+        isFavorite = favorite;
     }
 }
