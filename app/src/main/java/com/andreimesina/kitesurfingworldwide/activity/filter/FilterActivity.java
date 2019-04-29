adc068917691027a157ddef427ee644ccc943404
Andrei Mesina <andrei.mesina@my.fmi.unibuc.ro>
2019-04-29 20:50:25 +0300

    Implement Filter screen

--- app/src/main/java/com/andreimesina/kitesurfingworldwide/activity/filter/FilterActivity.java
+++ app/src/main/java/com/andreimesina/kitesurfingworldwide/activity/filter/FilterActivity.java
@@ -1,16 +1,66 @@
 package com.andreimesina.kitesurfingworldwide.activity.filter;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.andreimesina.kitesurfingworldwide.R;
+import com.andreimesina.kitesurfingworldwide.core.BaseActivity;
+import com.andreimesina.kitesurfingworldwide.data.model.SpotFilter;
 
-public class FilterActivity extends AppCompatActivity {
+public class FilterActivity extends BaseActivity {
+
+    private EditText editTextCountry;
+    private EditText editTextWindProbability;
+    private Button btnApply;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filter);
+
+        initToolbar(R.id.toolbar_layout_filter);
+        setBackButtonToolbar();
+        setTitle("Filter");
+
+        initViews();
+        listenOnApplyBtn();
+    }
+
+    private void initViews() {
+        editTextCountry = findViewById(R.id.editText_country_filter);
+        editTextWindProbability = findViewById(R.id.editText_windProbability_filter);
+        btnApply = findViewById(R.id.btn_apply_filter);
+
+        if (SpotFilter.getInstance().getCountry() != null) {
+            editTextCountry.setText(SpotFilter.getInstance().getCountry());
+        }
+
+        if (SpotFilter.getInstance().getWindProbability() != null) {
+            editTextWindProbability.setText(String.valueOf(SpotFilter.getInstance().getWindProbability()));
+        }
+    }
+
+    private void listenOnApplyBtn() {
+        btnApply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String country = editTextCountry.getText().toString();
+                String windProbability = editTextWindProbability.getText().toString();
+
+                SpotFilter.getInstance().setCountry(country);
+                SpotFilter.getInstance().setWindProbability(windProbability);
+
+                onBackPressed();
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        onBackPressed();
+        return super.onOptionsItemSelected(item);
     }
 }
