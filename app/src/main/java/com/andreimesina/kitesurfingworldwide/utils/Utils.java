1fd39a8a417306973b637b36ad7341e0d4a4bd6f
Andrei Mesina <andrei.mesina@my.fmi.unibuc.ro>
2019-04-29 20:53:18 +0300

    Add SharedPreferences util methods for later use

--- app/src/main/java/com/andreimesina/kitesurfingworldwide/utils/Utils.java
+++ app/src/main/java/com/andreimesina/kitesurfingworldwide/utils/Utils.java
@@ -1,9 +1,12 @@
 package com.andreimesina.kitesurfingworldwide.utils;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 
 public class Utils {
+
+    private static final String APP_KEY = "unbreakable";
 
     /**
      * Check if device is connected to a network.
@@ -14,5 +17,37 @@ public class Utils {
         ConnectivityManager cm = (ConnectivityManager) ctx.getSystemService(Context.CONNECTIVITY_SERVICE);
 
         return cm.getActiveNetworkInfo() != null;
+    }
+
+    // get a String value by key
+    public static String getString(Context ctx, String key) {
+        SharedPreferences sharedPreferences = ctx.getSharedPreferences(Utils.APP_KEY,
+                Context.MODE_PRIVATE);
+        return sharedPreferences.getString(key, "");
+    }
+
+    // save a String value by key
+    public static void setString(Context ctx, String key, String value) {
+        SharedPreferences sharedPreferences = ctx.getSharedPreferences(Utils.APP_KEY,
+                Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(key, value);
+        editor.apply();
+    }
+
+    // get a float value by key
+    public static float getFloat(Context ctx, String key) {
+        SharedPreferences sharedPreferences = ctx.getSharedPreferences(Utils.APP_KEY,
+                Context.MODE_PRIVATE);
+        return sharedPreferences.getFloat(key, 0.0f);
+    }
+
+    // save a float value by key
+    public static void setFloat(Context ctx, String key, float value) {
+        SharedPreferences sharedPreferences = ctx.getSharedPreferences(Utils.APP_KEY,
+                Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putFloat(key, value);
+        editor.apply();
     }
 }
